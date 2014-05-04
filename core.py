29e854db7afbf8138f5dd1d0bf1cf4a07c1a920a
Andrea Wu <andreaowu@berkeley.edu>
2014-05-04 08:27:56 -0700

    seventh chords added

--- core.py
+++ core.py
@@ -2,7 +2,7 @@
 from music21 import *
 from music21 import corpus
 from mapping import *
-	
+    
 chordToRoman = {}
 chordToRoman['0major'] = "I" 
 chordToRoman['1major'] = "II"
@@ -34,6 +34,7 @@ chordToRoman['5dim'] = "vio"
 chordToRoman['6dim'] = "viio"
 
 def generateFiguredBass(piece):
+    f = open('Analysis', 'w')
     key = piece.analyze('key')
     map = Mapping(str(key)).getMapper()
     numMeasures = len(piece.getElementsByClass('Part')[0].getElementsByClass('Measure'))
@@ -58,51 +59,98 @@ def generateFiguredBass(piece):
                             start = num
                             chord.append(map.get(elemName))
                             if num == len(st) - 1 and len(chord) != 0:
-                                print(chord)
-                                analyze(chord)
+                                analyze(chord, f)
                         elif found == 1:
                             found = 0
-                            print chord
-                            analyze(chord)
+                            analyze(chord, f)
                             break
         
-def analyze(chord):
+def analyze(chord, f):
     first = chord[len(chord) - 1]
     chord = list(set(chord))
     chord.sort()
     pos = chord.index(first)
     romanNumeral = ""
     if len(chord) == 3:
-        if chord[1] - chord[0] == 3:
-            if chord[2] - chord[1] == 3 or chord[2] - chord[1] == 6:
+        diffOne = chordDiff(chord, 0, 1)
+        diffTwo = chordDiff(chord, 1, 2)
+        if diffOne == 3:
+            if diffTwo == 3 or diffTwo == 6:
                 romanNumeral = chordToRoman[str(pos) + "dim"]
-            elif chord[2] - chord[1] == 4:
+            elif diffTwo == 4:
                 romanNumeral = chordToRoman[str(pos) + "minor"]
-            elif chord[2] - chord[1] == 5:
+            elif diffTwo == 5:
                 romanNumeral = chordToRoman[str(pos) + "major"]
-        elif chord[1] - chord[0] == 4:
-            if chord[2] - chord[1] == 3:
+        elif diffOne == 4:
+            if diffTwo == 3:
                 romanNumeral = chordToRoman[str(pos) + "major"]
-            elif chord[2] - chord[1] == 4:
+            elif diffTwo == 4:
                 romanNumeral = chordToRoman[str(pos) + "aug"]
-            elif chord[2] - chord[1] == 5:
+            elif diffTwo == 5:
                 romanNumeral = chordToRoman[str(pos) + "minor"]
-        elif chord[1] - chord[0] == 5:
-            if chord[2] - chord[1] == 4:
+        elif diffOne == 5:
+            if diffTwo == 4:
                 romanNumeral = chordToRoman[str(pos) + "major"]
-            elif chord[2] - chord[1] == 3:
+            elif diffTwo == 3:
                 romanNumeral = chordToRoman[str(pos) + "minor"]
-        elif chord[1] - chord[0] == 6:
-            if chord[2] - chord[1] == 4:
+        elif diffOne == 6:
+            if diffTwo == 4:
                 romanNumeral = chordToRoman[str(pos) + "aug"]
-            elif chord[2] - chord[1] == 3:
+            elif diffTwo == 3:
                 romanNumeral = chordToRoman[str(pos) + "dim"]
-    if pos == 1:
-        romanNumeral += " 6"
-    elif pos == 2:
-        romanNumeral += " 6 4"
-    print romanNumeral
+        if pos == 1:
+            romanNumeral += " 6"
+        elif pos == 2:
+            romanNumeral += " 6 4"
 
+    if len(chord) == 4:
+        diffOne = chordDiff(chord, 0, 1)
+        diffTwo = chordDiff(chord, 1, 2)
+        diffThree = chordDiff(chord, 2, 3)
+        if diffOne == 4:
+            if diffTwo == 3:
+                if diffThree == 4:
+                    romanNumeral = "Major V"
+                elif diffThree == 3:
+                    romanNumeral = "Dom V"
+                elif diffThree == 2:
+                    romanNumeral = "v"
+            elif diffTwo == 2:
+                if diffThree == 3:
+                    romanNumeral = "Half Dim v"
+            elif diffTwo == 1:
+                if diffThree == 4:
+                    romanNumeral = "Major V"
+        elif diffOne == 3:
+            if diffTwo == 4:
+                if diffThree == 3:
+                    romanNumeral = "v"
+                elif diffThree == 2:
+                    romanNumeral = "Half Dim v"
+                elif diffThree == 1:
+                    romanNumeral = "Major V"
+            elif diffTwo == 3:
+                if diffThree == 4:
+                    romanNumeral = "v"
+                elif diffThree == 3:
+                    romanNumeral = "Dim v"
+                elif diffThree == 2:
+                    romanNumeral = "Dom V"
+            elif diffTwo == 2:
+                if diffThree == 4:
+                    romanNumeral = "Dom V"
+        if pos == 1:
+            romanNumeral += "7"
+        elif pos == 2:
+            romanNumeral += " 6 5"
+        elif pos == 3:
+            romanNumeral += " 4 3"
+        elif pos == 4:
+            romanNumeral += " 2"
+    f.write(chord + ": " + romanNumeral + "\n")
+
+def chordDiff(chord, pos1, pos2):
+    return chord[pos2] - chord[pos1]
 
 generateFiguredBass(corpus.parse('bach/bwv7.7'))
 
